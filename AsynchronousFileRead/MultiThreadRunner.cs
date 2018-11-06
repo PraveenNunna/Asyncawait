9a3747eb79a85a25f00f93d142370de21438a848
Nunna, Praveen <Praveen.Nunna@harman.com>
2018-11-06 11:34:55 +0100

    Run multiple threads demo

---
+++ AsynchronousFileRead/MultiThreadRunner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsynchronousFileRead
+{
+    internal class MultiThreadRunner
+    {
+        public volatile bool cancelWorkerThread = false;
+
+        public void RunMultipleThreads()
+        {
+            var hiThread = new Thread(Sayhi);
+            hiThread.Name = "Print Thread";
+            hiThread.Start();
+            hiThread.Join();
+
+            var workerThread = new Thread(DoWork);
+            workerThread.Name = "Main Thread";
+            workerThread.Start();
+            Console.WriteLine("Press any key to exit main Thread");
+            Console.ReadKey();
+            cancelWorkerThread = true;
+            Console.WriteLine("Current thread name is : " + Thread.CurrentThread.Name);
+            workerThread.Join();
+
+            var byeThread = new Thread(SayBye);
+            byeThread.Name = "Bye Thread";
+            byeThread.Start("Bye");
+
+        }
+
+        private void DoWork()
+        {
+            while (!cancelWorkerThread)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"I am working right now.");
+            }
+        }
+
+        private void Sayhi()
+        {
+            Console.WriteLine($"Hello from: {Thread.CurrentThread.Name} ");
+        }
+
+        private void SayBye(object wish)
+        {
+            Console.WriteLine($"{wish.ToString()} from: {Thread.CurrentThread.Name} ");
+        }
+    }
+}
--- AsynchronousFileRead/Program.cs
+++ AsynchronousFileRead/Program.cs
@@ -10,18 +10,13 @@ namespace AsynchronousFileRead
     class Program
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program");
+
         static void Main(string[] args)
         {
-            var fileReader = new FileReader();
-            var task = fileReader.ReadFileAsync();
+            MultiThreadRunner runner = new MultiThreadRunner();
+            Thread.CurrentThread.Name = "Main Thread (Program)";
+            runner.RunMultipleThreads();
 
-            log.Debug("After ReadFileAsync in Program: {task.Result}");
-
-            fileReader.ReadSync();
-            log.Debug("After ReadSync in Program");
-
-
-            log.Debug($"After Program complete: {task.Result}");
             Console.ReadKey();
         }
     }
