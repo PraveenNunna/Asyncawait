46f861ab682fa21e6d990e0c5b5d95ee7f7d38ce
Nunna, Praveen <Praveen.Nunna@harman.com>
2018-11-06 11:02:20 +0100

    Initial commit.

---
+++ AsynchronousFileRead/FileReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AsynchronousFileRead
+{
+    class FileReader
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("FileReader");
+        public async Task<int> ReadFileAsync()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var fileStream = new FileStream("SampleFile.txt", FileMode.Open);
+            var buffer = new byte[fileStream.Length];
+            var totalBytesRead = fileStream.ReadAsync(buffer, 0, buffer.Length);
+
+            log.Debug("after ReadAsync call");
+            int length = await totalBytesRead;
+            sw.Stop();
+            Thread.Sleep(2000);
+
+            log.Debug($"Total time taken to read file async is: {sw.Elapsed.TotalSeconds}");
+            return length;
+        }
+
+        public int ReadSync()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var fileStream = new FileStream("SampleFile1.txt", FileMode.Open);
+            var buffer = new byte[fileStream.Length];
+            var totalBytesRead = fileStream.Read(buffer, 0, buffer.Length);
+
+            log.Debug("After read call");
+
+            sw.Stop();
+
+            log.Debug($"Total time taken to read file sync is: {sw.Elapsed.TotalSeconds}");
+
+            return totalBytesRead;
+        }
+    }
+}
--- AsynchronousFileRead/Program.cs
+++ AsynchronousFileRead/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace AsynchronousFileRead
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program");
         static void Main(string[] args)
         {
+            var fileReader = new FileReader();
+            var task = fileReader.ReadFileAsync();
+
+            log.Debug("After ReadFileAsync in Program: {task.Result}");
+
+            fileReader.ReadSync();
+            log.Debug("After ReadSync in Program");
+
+
+            log.Debug($"After Program complete: {task.Result}");
+            Console.ReadKey();
         }
     }
 }
