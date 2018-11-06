82174c2d17f1211fed27bf46e1f985dc745c8c43
Nunna, Praveen <Praveen.Nunna@harman.com>
2018-11-06 15:21:40 +0100

    using volatile keyword.

--- AsynchronousFileRead/Program.cs
+++ AsynchronousFileRead/Program.cs
@@ -7,22 +7,39 @@ using System.Threading;
 
 namespace AsynchronousFileRead
 {
+    //Run in release mode.
+    //Remove and keep volatile keyword for _loop variable.
     class Program
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program");
+
+        //volatile bool cancelThread = true;
+
+        bool cancelThread = true;
+
         static void Main(string[] args)
         {
-            var fileReader = new FileReader();
-            var task = fileReader.ReadFileAsync();
+            Program program = new Program();
 
-            log.Debug("After ReadFileAsync in Program: {task.Result}");
-
-            fileReader.ReadSync();
-            log.Debug("After ReadSync in Program");
-
-
-            log.Debug($"After Program complete: {task.Result}");
+            Thread workerThread = new Thread(DoWork);
+            workerThread.Start(program);
+            Console.WriteLine("Enter any key to cancel work.");
             Console.ReadKey();
+
+            program.cancelThread = false;
+            Console.WriteLine("Value Set to false");
+        }
+
+        private static void DoWork(object program)
+        {
+            Program prog = (Program)program;
+            Console.WriteLine("Loop Starting...");
+            while (prog.cancelThread)
+            {
+                //Thread.Sleep(1000);
+                //Console.WriteLine("I am working....`");
+            }
+            Console.WriteLine("Loop Stopping...");
         }
     }
 }
